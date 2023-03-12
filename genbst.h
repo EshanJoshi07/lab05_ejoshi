4576b76f422d85f1a84320f0a8a7c024d9797889
Eshan Joshi <eshanjoshi@cs.ucsb.edu>
2023-03-12 15:13:21 -0700

    working code

---
+++ genbst.h
@@ -0,0 +1,55 @@
+//genbst.h
+//NAME: ESHAN JOSHI
+
+#ifndef GENBST_H
+#define GENBST_H
+
+using namespace std;
+
+// Replace the following existing definition of GenBST with the new class definition
+template <class T>
+class GenBST {
+    public:
+        GenBST();
+        ~GenBST(); 
+        bool insert(T value);
+        void printPreOrder() const;
+        void printInOrder() const;
+        void printPostOrder() const;
+        T sum() const;
+        int count() const;
+        bool contains(T value) const;
+        T getPredecessor(T value) const;
+        T getSuccessor(T value) const;
+        bool remove(T value);
+        // int minVal() const;
+        // int maxVal() const;
+
+    private:
+        struct Node {
+            T info;
+            Node *left, *right, *parent;
+            Node (T v = 0) : info(v), left(0), right(0), parent(0) {}
+        };
+        Node *root;
+        Node* getNodeFor(T value, Node* n) const;
+        void clear(Node *n);
+        bool insert(T value, Node *n);
+        void printPreOrder(Node *n) const;
+        void printInOrder(Node *n) const;
+        void printPostOrder(Node *n) const;
+        T sum(Node *n) const;
+        int count(Node *n) const;
+        Node* getSuccessorNode(T value) const;
+        Node* getPredecessorNode(T value) const;
+        
+};
+
+#include genbst.cpp
+
+#endif
+
+// template <typename T>
+// inline GenBST<T>::GenBST()
+// {
+// }
